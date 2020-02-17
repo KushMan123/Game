d3a25508ead9b6858d62ef4077541ab5ff5b6604
AronSrestha <aronshrestha498@gmail.com>
2020-02-17 15:31:13 +0545

    Aron

--- Character/Player/Player_abstract.gd
+++ Character/Player/Player_abstract.gd
@@ -1,10 +1,7 @@
 extends KinematicBody2D
 class_name Player
 
-var health = 1200
-var plane_var= 0
 
-onready var tween= get_node("Tween")
 
 func in_which_plane():
 	print("The function is not implemented in %s" %filename)
