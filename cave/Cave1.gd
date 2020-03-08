b19162aff539d711a68b0b80ef03a3e2bfc5b3ce
Ishar Maharjan <isharmaharjan123@gmail.com>
2020-03-08 09:38:31 +0545

    sound added to scene 3 and 4 & NPC added

--- cave/Cave1.gd
+++ cave/Cave1.gd
@@ -1,9 +1,16 @@
 extends Area2D
 
+var enter = false
+
 func _ready():
 	pass
 
+func _process(delta):
+	print("CAVE", enter)
+	if enter:
+		get_tree().change_scene("res://stage/xy_A/Stage.tscn")
 
 func _on_Cave1_body_entered(body):
-	if body is Player_in_xz:
-		get_tree().change_scene("res://stage/xy_A/Stage.tscn") 
+	if body is Player:
+		$AudioStreamPlayer2D.play()
+		enter = true
